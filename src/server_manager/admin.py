711794c0fbd1365ffbf277fdd1b6336760b2ec66
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-07-30 02:07:29 +0100

    Registred AntiCheatConfigTemplates & AntiCheatConfigurations to admin dashboard

--- src/server_manager/admin.py
+++ src/server_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AntiCheatConfigTemplates, AntiCheatConfigurations
 
-# Register your models here.
+
+admin.site.register(AntiCheatConfigTemplates)
+admin.site.register(AntiCheatConfigurations)

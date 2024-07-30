c639ffc7f53bf64c607f71ab093036532915898e
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-07-30 02:06:45 +0100

    Added AntiCheatConfigTemplates & AntiCheatConfigurations models

--- src/server_manager/models.py
+++ src/server_manager/models.py
@@ -1,3 +1,28 @@
+from shared.models import ServerTypes
+
 from django.db import models
 
-# Create your models here.
+
+class AntiCheatConfigTemplates(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(null=True, default=None, blank=True)
+    type = models.IntegerField(
+        choices=ServerTypes, null=False, default=ServerTypes.MTASA
+    )
+
+    class Meta:
+        db_table = "anticheat_config_templates"
+        verbose_name = "AntiCheat Config Template"
+        verbose_name_plural = "AntiCheat Config Templates"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class AntiCheatConfigurations(models.Model):
+    config = models.TextField()  # Json Dump
+
+    class Meta:
+        db_table = "anticheat_configurations"
+        verbose_name = "AntiCheat Configuration"
+        verbose_name_plural = "AntiCheat Configurations"

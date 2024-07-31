95ad3952746866b75131e0d8c62f2c7b4ae197fe
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-07-31 02:27:48 +0100

    Register dashboard context preprocessor

--- src/EagleAntiCheat/settings.py
+++ src/EagleAntiCheat/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                
+                "dashboard.context_preprocessor.preprocess_notification_pins"
             ],
         },
     },

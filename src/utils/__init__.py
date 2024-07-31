30ae266d14c79451eddd2e1862645d7eed73822a
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-07-31 15:40:51 +0100

    Added check_request_body_key utils function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,6 @@
 from string import ascii_uppercase, digits
 from random import shuffle
-from typing import Optional
+from typing import Optional, Dict, Any
 
 
 def generate_key(parts: Optional[int] = 4) -> str:
@@ -15,3 +15,25 @@ def generate_key(parts: Optional[int] = 4) -> str:
         license_key += "".join(allowed_chars)[:5] + "-"
 
     return license_key[:-1]
+
+def check_request_body_key(request_body: Dict[str, Any], target_key: str, key_type: type) -> bool:
+    """Check a key in a request body
+
+    Args:
+    -----
+        request_body (Dict[str, Any]): The request body
+        target_key (str): The key to check
+        key_type (type): The key type
+
+    Returns:
+    --------
+        bool: True if the key exists and it's type comptatible else False
+    """    
+    if not target_key in request_body.keys():
+        print(f"{target_key} Not Found in request. (given request: {request_body})")
+        return False
+    
+    if not isinstance(request_body[target_key], key_type):
+        print(f"'{target_key}''s type isn't comptatible with {key_type}")
+        return False
+    return True

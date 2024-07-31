77cbe79c5e633246e7c1c260c9254aa0217e7f1a
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-07-31 21:09:49 +0100

    Update AntiCheatConfigurations model and AntiCheatConfigTemplates model
    Added default value to the anticheat config template model
    Switched config data type from TextField to JSONField

--- src/server_manager/models.py
+++ src/server_manager/models.py
@@ -1,7 +1,6 @@
 from shared.models import ServerTypes
-
 from django.db import models
-
+from typing import Union
 
 class AntiCheatConfigTemplates(models.Model):
     class AntiCheatConfigDataTypes(models.IntegerChoices):
@@ -19,18 +18,31 @@ class AntiCheatConfigTemplates(models.Model):
         null=False,
         default=AntiCheatConfigDataTypes.BOOLEAN,
     )
+    default_value = models.TextField(blank=True)
 
     class Meta:
         db_table = "anticheat_config_templates"
         verbose_name = "AntiCheat Config Template"
         verbose_name_plural = "AntiCheat Config Templates"
+        
+    def get_default_value(self) -> Union[bool, str, int]:
+        """Convert `default_value` to the correct type based on `config_type`."""
+        if self.config_type == 1:
+            return self.default_value.strip().lower() in ('true', '1')
+        elif self.config_type == 2:
+            return self.default_value
+        elif self.config_type == 3:
+            return int(self.default_value)
+        # Handle other types
+        return self.default_value
+
 
     def __str__(self) -> str:
         return self.name
 
 
 class AntiCheatConfigurations(models.Model):
-    config = models.TextField()  # Json Dump
+    config = models.JSONField(blank=False, default=dict)  # Json Dump
 
     class Meta:
         db_table = "anticheat_configurations"

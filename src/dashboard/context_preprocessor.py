757299e0d050829c94dceb08d727afa2b4144312
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-04 16:11:16 +0100

    Added sidebar CTAs

--- src/dashboard/context_preprocessor.py
+++ src/dashboard/context_preprocessor.py
@@ -1,14 +1,25 @@
 from django.http import HttpRequest
-from dashboard.models import PatchNotes, Announcements
+from dashboard.models import PatchNotes, Announcements, GameServers
 from typing import Dict, Any
 
 
 def preprocess_sidebar_context(request: HttpRequest) -> Dict[str, Any]:
     if request.user.is_authenticated:
+        cta_pins = []
+        if GameServers.objects.filter(owner=request.user).count() == 0:
+            cta_pins.append(
+                {
+                    "title": "Subscription",
+                    "description": "No servers found on your account. Please subscribe your server to access those options",
+                    "extra_url": {"text": "Subscribe Now", "url": "/dashboard/servers"},
+                }
+            )
+
         return {
             "unseen_patchnotes": PatchNotes.objects.exclude(seens=request.user).count(),
             "unseen_announcements": Announcements.objects.exclude(
                 seens=request.user
             ).count(),
+            "cta_pins": cta_pins
         }
     return {}

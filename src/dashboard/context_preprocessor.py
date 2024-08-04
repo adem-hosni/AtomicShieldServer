bdd7fa7aeffc92e4c61767638ee7bfb08b37f9dc
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-04 16:00:41 +0100

    Compress context preprocessors

--- src/dashboard/context_preprocessor.py
+++ src/dashboard/context_preprocessor.py
@@ -3,19 +3,12 @@ from dashboard.models import PatchNotes, Announcements
 from typing import Dict, Any
 
 
-def preprocess_patchnote_notifpins(request: HttpRequest) -> Dict[str, Any]:
+def preprocess_sidebar_context(request: HttpRequest) -> Dict[str, Any]:
     if request.user.is_authenticated:
         return {
-            "unseen_patchnotes": PatchNotes.objects.exclude(seens=request.user).count()
-        }
-    return {}
-
-
-def preprocess_announcement_notifpins(request: HttpRequest) -> Dict[str, Any]:
-    if request.user.is_authenticated:
-        return {
+            "unseen_patchnotes": PatchNotes.objects.exclude(seens=request.user).count(),
             "unseen_announcements": Announcements.objects.exclude(
                 seens=request.user
-            ).count()
+            ).count(),
         }
     return {}

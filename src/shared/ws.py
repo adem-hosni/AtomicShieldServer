5ed5aceef77f5516935c7464725adefe31ff14bc
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-06 16:50:38 +0100

    Added EagleScannerPacketID enum

--- src/shared/ws.py
+++ src/shared/ws.py
@@ -6,6 +6,10 @@ class EagleServerPacketID(Enum):
     SYNC_ANTICHEAT_CONFIGS = 2
 
 
+class EagleScannerPacketID(Enum):
+    NETWORK_JOIN = 1
+    SYNC_SIGNATURES = 2
+
 class WebSocketGroupNames(Enum):
     EAGLE_SERVERS = "EAGLE_SERVERS"
     EAGLE_CLIENTSCANNER = "EAGLE_CLIENTSCANNER"

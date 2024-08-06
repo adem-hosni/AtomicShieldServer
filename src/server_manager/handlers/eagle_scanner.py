c227dbaaefded36c57356ad32b939245ed943247
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-06 16:51:29 +0100

    Added eagle scanner consumer handling functions

---
+++ src/server_manager/handlers/eagle_scanner.py
@@ -0,0 +1,30 @@
+from ..consumers.eagle_scanner import EagleScanner
+from shared.ws import WebSocketGroupNames, EagleScannerPacketID
+from asgiref.sync import sync_to_async
+from ..models import MaliciousSignatures, ServerTypes
+from typing import Dict, Any
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+async def handle_network_join(consumer: EagleScanner, request: Dict[str, Any]):
+    logger.warning(
+        f"{consumer.address[0]}:{consumer.address[1]} agent asking for network join..."
+    )
+    consumer.group_name = WebSocketGroupNames.EAGLE_CLIENTSCANNER.value
+    consumer.channel_layer.group_add(consumer.group_name, consumer.channel_name)
+    logger.info(
+        f"{consumer.address[0]}:{consumer.address[1]} agent joined network successfuly!"
+    )
+    
+async def handle_signatures_sync(consumer: EagleScanner, request: Dict[str, Any]):
+    signatures = await sync_to_async(list)(MaliciousSignatures.objects.filter(type=ServerTypes.MTASA))
+    await consumer.send({
+        "type": EagleScannerPacketID.SYNC_SIGNATURES.value,
+        "signatures": {
+            signature.name: signature.signatures for signature in signatures
+        }
+    })
+    logger.info(f"{consumer.address[0]}:{consumer.address[1]} Signatures Synced Successfuly!")

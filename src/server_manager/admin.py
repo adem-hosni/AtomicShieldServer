2f8d65f37bacf1aeda0c0bc5a1f6aa11d7b6d6ad
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-06 16:52:13 +0100

    Register MaliciousSignatures in admin dashboard

--- src/server_manager/admin.py
+++ src/server_manager/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AntiCheatConfigTemplates, AntiCheatConfigurations
+from .models import AntiCheatConfigTemplates, AntiCheatConfigurations, MaliciousSignatures
 
 
 admin.site.register(AntiCheatConfigTemplates)
 admin.site.register(AntiCheatConfigurations)
+admin.site.register(MaliciousSignatures)

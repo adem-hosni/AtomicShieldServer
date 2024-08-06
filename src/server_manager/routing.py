2d5b919ac9e01ba7be01d65468e7dd3d540f0846
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-06 16:50:49 +0100

    Added new websocket route

--- src/server_manager/routing.py
+++ src/server_manager/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .consumers import eagle_server
+from .consumers import eagle_server, eagle_scanner
 
 
 websocket_urlpatterns = [
     re_path(r"^c/eagleserver/$", eagle_server.EagleServerConsumer.as_asgi()), 
+    re_path(r"^c/eaglescanner/$", eagle_scanner.EagleScanner.as_asgi()), 
 ]

aeae7bde6d71130c06318656666256799ae6a981
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-11 19:03:32 +0100

    Register AntiCheatConfigurationCategories to admin dashboard

--- src/server_manager/admin.py
+++ src/server_manager/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import AntiCheatConfigTemplates, AntiCheatConfigurations, MaliciousSignatures
+from .models import (
+    AntiCheatConfigTemplates,
+    AntiCheatConfigurations,
+    AntiCheatConfigurationCategories,
+    MaliciousSignatures,
+)
 
 
+admin.site.register(AntiCheatConfigurationCategories)
 admin.site.register(AntiCheatConfigTemplates)
 admin.site.register(AntiCheatConfigurations)
 admin.site.register(MaliciousSignatures)

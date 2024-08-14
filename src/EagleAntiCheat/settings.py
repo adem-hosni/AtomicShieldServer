686485b7c3840bc98d755f2e1395a9e58651a17f
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-14 16:23:38 +0100

    Update settings.py

--- src/EagleAntiCheat/settings.py
+++ src/EagleAntiCheat/settings.py
@@ -35,10 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # "channels",
     "daphne",
-    # "jazzmin",
-    # "unfold",
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

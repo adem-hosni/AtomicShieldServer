1f46c1493d07e700175fe4a592e613966651871c
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-25 01:12:31 +0100

    Update routing.py

--- src/server_manager/routing.py
+++ src/server_manager/routing.py
@@ -3,6 +3,6 @@ from .consumers import eagle_server, eagle_scanner
 
 
 websocket_urlpatterns = [
-    path("c/eaglecserver/", eagle_server.EagleServerConsumer.as_asgi()), 
+    path("c/eagleserver/", eagle_server.EagleServerConsumer.as_asgi()), 
     path("c/eaglescanner/", eagle_scanner.EagleScanner.as_asgi()), 
 ]

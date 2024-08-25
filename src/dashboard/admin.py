213ce06dcd16050e9f44cc8a68ec59c94c21fdd2
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-25 22:20:40 +0100

    Implemented GameServer model admin

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import GameServers, Announcements, PatchNotes
 
-admin.site.register(GameServers)
+
+class GameServerAdmin(admin.ModelAdmin):
+    list_display = ["address", "owner", "type"]
+    
+    @admin.display(description="Address")
+    def address(self, obj: GameServers):
+        return f"{obj.ip}:{obj.port}"
+
+    @admin.display(description="Owner")
+    def address(self, obj: GameServers):
+        return f"{obj.owner.username}"
+
+    @admin.display(description="Type")
+    def address(self, obj: GameServers):
+        return f"{obj.type}"
+
+admin.site.register(GameServers, GameServerAdmin)
 admin.site.register(Announcements)
 admin.site.register(PatchNotes)

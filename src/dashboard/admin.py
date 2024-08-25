358ff9b01a4c25b439149f5d60f81dcc4fdcffd1
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-25 22:37:56 +0100

    Update admin.py

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -5,16 +5,17 @@ from .models import GameServers, Announcements, PatchNotes
 class GameServerAdmin(admin.ModelAdmin):
     list_display = ["address", "owner", "type"]
 
+
     @admin.display(description="Address")
     def address(self, obj: GameServers):
         return f"{obj.ip}:{obj.port}"
 
     @admin.display(description="Owner")
-    def address(self, obj: GameServers):
+    def owner(self, obj: GameServers):
         return f"{obj.owner.username}"
 
     @admin.display(description="Type")
-    def address(self, obj: GameServers):
+    def type(self, obj: GameServers):
         return f"{obj.type}"
 
 

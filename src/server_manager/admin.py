aa6308ce649f8ea0dddf19ed6b7d13784580b38d
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-25 22:33:52 +0100

    Implement ClientHWIDs model admin

--- src/server_manager/admin.py
+++ src/server_manager/admin.py
@@ -8,8 +8,28 @@ from .models import (
 )
 
 
+class ClientHWIDAdmin(admin.ModelAdmin):
+    list_display = ["username", "serial", "motherboard_serial", "bios_version"]
+
+    @admin.display(description="Username")
+    def username(self, obj: ClientHWIDS):
+        return obj.username
+
+    @admin.display(description="Serial")
+    def serial(self, obj: ClientHWIDS):
+        return obj.mta_serial
+
+    @admin.display(description="Motherboard Serial")
+    def motherboard_serial(self, obj: ClientHWIDS):
+        return obj.motherboard_serial
+
+    @admin.display(description="BOIS Version")
+    def motherboard_serial(self, obj: ClientHWIDS):
+        return obj.bios_version
+
+
 admin.site.register(AntiCheatConfigurationCategories)
 admin.site.register(AntiCheatConfigTemplates)
 admin.site.register(AntiCheatConfigurations)
 admin.site.register(MaliciousSignatures)
-admin.site.register(ClientHWIDS)
+admin.site.register(ClientHWIDS, ClientHWIDAdmin)

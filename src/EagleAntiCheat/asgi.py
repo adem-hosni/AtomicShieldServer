777d0d5cba768b17f157b1348e982cdef030afa9
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-25 01:05:16 +0100

    Fixed app registry error

--- src/EagleAntiCheat/asgi.py
+++ src/EagleAntiCheat/asgi.py
@@ -12,13 +12,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+django_asgi_app = get_asgi_application()
+
 import server_manager.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EagleAntiCheat.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(server_manager.routing.websocket_urlpatterns)
         ),

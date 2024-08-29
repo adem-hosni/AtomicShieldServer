bf8413d562cc0b84ac0db98b5b4e976e58fde933
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-08-29 12:23:14 +0100

    Update apps.py

--- src/server_manager/apps.py
+++ src/server_manager/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ServerManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'server_manager'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "server_manager"
+    verbose_name = "Server manager"

05f80a14be327d3577f413b66fb71addd857ff93
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-01 12:47:43 +0100

    Add agent peb download view

--- src/resourcehub/views.py
+++ src/resourcehub/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def download_agent_peb(request: HttpRequest) -> HttpResponse:
+    with open(
+        f"../bin/{'debug' if settings.DEBUG else 'production'}/agent/agent_peb.dll",
+        "rb",
+    ) as file:
+        agent_peb_buffer = file.read()
+
+    return HttpResponse(agent_peb_buffer, content_type="application/octet-stream")

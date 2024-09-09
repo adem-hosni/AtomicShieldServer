188126f8707f112ad5cfcf5846f372397b33de4b
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-09 14:10:55 +0100

    Document eagle scanner class

--- src/anticheat/consumers/eagle_scanner.py
+++ src/anticheat/consumers/eagle_scanner.py
@@ -6,21 +6,44 @@ import json
 from typing import Union, Dict, List, Optional, Any
 import logging
 
-
 logger = logging.getLogger(__name__)
 
 
 class EagleScanner(AsyncWebsocketConsumer):
+    """
+    An asynchronous WebSocket consumer designed to handle communication between a client and a server.
+
+    Attributes:
+    -----------
+    _group_name (str): The name of the WebSocket group.
+    _hwid (ClientHWID): The hardware ID of the client.
+    _connected_server (EagleServerConsumer): The connected server instance.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes the EagleScanner instance.
+
+        Args:
+        -----
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self._group_name = ""
         self._hwid: ClientHWID = None
         self._connected_server: EagleServerConsumer = None
 
     async def connect(self):
+        """
+        Accepts the WebSocket connection and logs the client's IP address.
+
+        Returns:
+        --------
+            None
+        """
         await self.accept()
         self.address = tuple(self.scope["client"])
-
         logger.info(
             f"{self.address[0]}:{self.address[1]} Agent asking for connection..."
         )
@@ -56,10 +79,28 @@ class EagleScanner(AsyncWebsocketConsumer):
 
     @property
     def group_name(self) -> str:
+        """
+        Gets the group name.
+
+        Returns:
+        --------
+            str: The group name.
+        """
         return self._group_name
 
     @group_name.setter
     def group_name(self, value: Any):
+        """
+        Sets the group name.
+
+        Args:
+        -----
+            value (Any): The new group name. Must be a string.
+
+        Raises:
+        ------
+            TypeError: If the value is not a string.
+        """
         if not isinstance(value, str):
             raise TypeError(
                 f"Invalid room group name value, expected 'User' got {type(value)}"
@@ -67,6 +108,18 @@ class EagleScanner(AsyncWebsocketConsumer):
         self._group_name = value
 
     async def receive(self, text_data=None, bytes_data=None):
+        """
+        Handles incoming WebSocket messages.
+
+        Args:
+        -----
+            text_data (str): The incoming text data.
+            bytes_data (bytes): The incoming bytes data.
+
+        Returns:
+        --------
+            None
+        """
         try:
             # Attempt to parse the incoming message as JSON
             request_body: Dict[str, Any] = json.loads(text_data)
@@ -101,6 +154,17 @@ class EagleScanner(AsyncWebsocketConsumer):
                 await handle_malicious_signature_detected(self, request_body)
 
     async def disconnect(self, code):
+        """
+        Handles the disconnection of the WebSocket.
+
+        Args:
+        -----
+            code: The disconnection code.
+
+        Returns:
+        --------
+            Awaitable: An awaitable object for the disconnect operation.
+        """
         if self._group_name == WebSocketGroupNames.EAGLE_CLIENTSCANNER.value:
             from ..handlers.eagle_scanner import handle_scanner_disconnect
 
@@ -110,20 +174,56 @@ class EagleScanner(AsyncWebsocketConsumer):
 
     @property
     def hwid(self):
+        """
+        Gets the hardware ID.
+
+        Returns:
+        --------
+            ClientHWID: The hardware ID.
+        """
         return self._hwid
 
     @hwid.setter
     def hwid(self, hwid: Union[ClientHWID, Any]):
+        """
+        Sets the hardware ID.
+
+        Args:
+        -----
+            hwid (Union[ClientHWID, Any]): The new hardware ID. Must be of type ClientHWID.
+
+        Raises:
+        ------
+            TypeError: If the value is not of type ClientHWID.
+        """
         if not isinstance(hwid, ClientHWID):
             raise TypeError(f"Can't convert type {type(hwid)} to type ClientHWIDs")
         self._hwid = hwid
 
     @property
     def connected_server(self) -> EagleServerConsumer:
+        """
+        Gets the connected server.
+
+        Returns:
+        --------
+            EagleServerConsumer: The connected server instance.
+        """
         return self._connected_server
 
     @connected_server.setter
     def connected_server(self, server: Union[EagleServerConsumer, None]):
+        """
+        Sets the connected server.
+
+        Args:
+        -----
+            server (Union[EagleServerConsumer, None]): The new connected server instance. Must be of type EagleServerConsumer.
+
+        Raises:
+        ------
+            TypeError: If the value is not of type EagleServerConsumer.
+        """
         if not isinstance(server, EagleServerConsumer):
             raise TypeError(
                 f"Unable to convert type {type(server)} to 'EagleServerConsumer'"
@@ -131,6 +231,17 @@ class EagleScanner(AsyncWebsocketConsumer):
         self._connected_server = server
 
     async def kick(self, reason: Optional[str] = "") -> bool:
+        """
+        Kicks a client by sending a PLAYER_KICK packet to the connected server.
+
+        Args:
+        -----
+            reason (Optional[str]): The reason for kicking the client.
+
+        Returns:
+        --------
+            bool: True if the kick was successful, False otherwise.
+        """
         if self._connected_server:
             await self._connected_server.send(
                 EagleServerPacketID.PLAYER_KICK,

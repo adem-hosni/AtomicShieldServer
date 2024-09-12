86f7939b5f6ba73b0ba4ad6a765f1ee8b1eb6805
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-12 17:48:03 +0100

    Update apps.py

--- src/anticheat/apps.py
+++ src/anticheat/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ServerManagerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "anticheat"
-    verbose_name = "AntiCheat"
+    verbose_name = "Engine"

19a8371c209ac26a153648f5db911a35079b3a45
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-10-02 17:55:09 +0100

    Update utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,7 @@
 from string import ascii_uppercase, digits
 from random import shuffle
 from typing import Optional, Dict, Any
+from datetime import timedelta
 
 
 def generate_key(parts: Optional[int] = 4) -> str:
@@ -16,7 +17,10 @@ def generate_key(parts: Optional[int] = 4) -> str:
 
     return license_key[:-1]
 
-def check_request_body_key(request_body: Dict[str, Any], target_key: str, key_type: type) -> bool:
+
+def check_request_body_key(
+    request_body: Dict[str, Any], target_key: str, key_type: type
+) -> bool:
     """Check a key in a request body
 
     Args:
@@ -28,12 +32,30 @@ def check_request_body_key(request_body: Dict[str, Any], target_key: str, key_ty
     Returns:
     --------
         bool: True if the key exists and it's type comptatible else False
-    """    
+    """
     if not target_key in request_body.keys():
         print(f"{target_key} Not Found in request. (given request: {request_body})")
         return False
-    
+
     if not isinstance(request_body[target_key], key_type):
         print(f"'{target_key}''s type isn't comptatible with {key_type}")
         return False
     return True
+
+
+def represent_timedelta_string(time: timedelta) -> str:
+    result = ""
+
+    days = time.days
+    total_seconds = time.seconds
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+
+    if days > 0:
+        result += f"{days} days "
+    if hours > 0:
+        result += f"{hours}h "
+    if minutes > 0:
+        result += f"{minutes}mn "
+
+    return result[:-1]

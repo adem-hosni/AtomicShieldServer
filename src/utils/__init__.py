bf02c1b4bfcb9aa3e12a702cae4f54e279c28ce8
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-10-12 22:06:48 +0100

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
+import re
 from string import ascii_uppercase, digits
 from random import shuffle
 from typing import Optional, Dict, Any
@@ -36,7 +37,9 @@ def check_request_body_key(
         return False
 
     if not isinstance(request_body[target_key], key_type):
-        print(f"'{target_key}''s type isn't comptatible with {key_type} (got: {request_body[target_key]})")
+        print(
+            f"'{target_key}''s type isn't comptatible with {key_type} (got: {request_body[target_key]})"
+        )
         return False
     return True
 
@@ -57,3 +60,29 @@ def represent_timedelta_string(time: timedelta) -> str:
         result += f"{minutes}mn "
 
     return result[:-1]
+
+
+def isvalid_ip(ip: str) -> bool:
+    """
+    Validate if the given string is a valid IPv4 address.
+
+    An IPv4 address consists of four decimal numbers, each ranging from 0 to 255, separated by dots.
+    This function checks if the provided IP address follows the correct format and is within the allowed range.
+
+    Args:
+    -----
+        ip (str): The string representing an IPv4 address.
+
+    Returns:
+    --------
+        bool: True if the provided string is a valid IPv4 address, otherwise False.
+
+    Notes:
+    ------
+        This function uses a regular expression to validate the IPv4 address. The regular expression allows
+        for addresses like '0.0.0.0' or '255.255.255.255', and enforces each segment to be in the range 0-255.
+    """
+    ipv4_pattern = re.compile(
+        r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    )
+    return ipv4_pattern.match(ip)

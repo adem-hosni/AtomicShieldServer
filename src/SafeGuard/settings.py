0f388563621285141831a880abeb9e595a0a4f33
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-10-12 22:05:30 +0100

    Update settings.py

--- src/SafeGuard/settings.py
+++ src/SafeGuard/settings.py
@@ -199,6 +199,27 @@ ANTICHEAT_NAME_LONG = f"{ANTICHEAT_NAME} {ANTICHEAT_VERSION}"
 UNFOLD = {
     "SITE_HEADER": f"{ANTICHEAT_NAME} STAFF",
     "SITE_SYMBOL": "shield",
+    "SIDEBAR": {
+        "show_search": True,
+        "show_all_applications": True,
+    },
+    "COLORS": {
+        "primary": {
+            "50": "250 245 255",
+            "100": "243 232 255",
+            "200": "233 213 255",
+            "300": "216 180 254",
+            "400": "192 132 252",
+            "500": "168 85 247",
+            "600": "140 89 43",
+            "700": "126 34 206",
+            "800": "107 33 168",
+            "900": "88 28 135",
+            "950": "59 7 100",
+        },
+    },
+    # Other configurations...
+
 }
 
 # Authentication Settings

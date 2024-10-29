b7449846406a0866d4082678556369195c3da6a2
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-10-29 23:30:27 +0100

    Update websocket routes

--- src/anticheat/routing.py
+++ src/anticheat/routing.py
@@ -3,6 +3,6 @@ from .consumers import safe_engine, safe_server
 
 
 websocket_urlpatterns = [
-    path("c/eagleserver/", safe_server.SafeServerConsumer.as_asgi()), 
-    path("c/eaglescanner/", safe_engine.SafeEngineConsumer.as_asgi()), 
+    path("c/safeguardserver/", safe_server.SafeServerConsumer.as_asgi()), 
+    path("c/safeguardagent/", safe_engine.SafeEngineConsumer.as_asgi()), 
 ]

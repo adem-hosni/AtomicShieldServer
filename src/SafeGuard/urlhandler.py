028a61778aa21b1819a88a420e7d9d8e5d83bcb1
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-11-09 20:38:42 +0100

    Update urlhandler.py

--- src/SafeGuard/urlhandler.py
+++ src/SafeGuard/urlhandler.py
@@ -2,5 +2,5 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 
-def handler404(request: HttpRequest, exception, template_name: str = None):
+def handler404(request: HttpRequest, exception: Exception, template_name: str = None):
     return render(request, "pages/404.jinja", {})

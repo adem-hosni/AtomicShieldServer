ef5fb6ab7d5f7e6f473d767c903acc54b242554d
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-11-15 15:03:57 +0100

    Update asgi.py

--- src/SafeGuard/asgi.py
+++ src/SafeGuard/asgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SafeGuard.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack

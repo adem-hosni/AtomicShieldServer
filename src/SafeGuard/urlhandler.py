925934789e3e748181cdaa11aab43ba41b692b96
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-11-16 21:24:23 +0100

    Update urlhandler.py

--- src/SafeGuard/urlhandler.py
+++ src/SafeGuard/urlhandler.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def handler404(request: HttpRequest, exception: Exception, template_name: str = None):
-    return render(request, "pages/404.jinja", {})
+    return render(request, "pages/404.jinja", {}, status=404)

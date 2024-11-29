d8ac9c9003ff4990e169e0189548774f8a410345
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-11-29 14:08:49 +0100

    Add fivem guard management support

--- src/guards/__init__.py
+++ src/guards/__init__.py
@@ -1,1 +1,2 @@
 from .multitheftauto import mta_guard
+from .fivem import fivem_guard
---
+++ src/guards/fivem.py
@@ -0,0 +1,11 @@
+from ._base import _GuardManagerBase
+
+
+class FivemGuardManager(_GuardManagerBase):
+    """
+    FivemGuardManager is responsible for managing SafeGuard servers and engines.
+    It provides functionalities to add, remove, and check the status of servers and engines.
+    """
+
+
+fivem_guard = FivemGuardManager()

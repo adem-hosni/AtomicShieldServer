1ed666aedfd26226e5508ef9c2cf7b5de20ae716
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-15 22:25:27 +0100

    Upgrade from deprecated function warn() to warnnig()

--- src/anticheat/consumers/safe_engine.py
+++ src/anticheat/consumers/safe_engine.py
@@ -139,19 +139,19 @@ class SafeEngineConsumer(AsyncWebsocketConsumer):
             # Attempt to parse the incoming message as JSON
             request_body: Dict[str, Any] = json.loads(text_data)
         except json.decoder.JSONDecodeError:
-            logger.warn(f"Failed to parse request. (request body: {request_body})")
+            logger.warning(f"Failed to parse request. (request body: {request_body})")
             return self.close()
 
         # Check if the request body contains a 'type' key
         if not "type" in request_body.keys():
-            logger.warn(f"Failed to get request type. (given request: {request_body})")
+            logger.warning(f"Failed to get request type. (given request: {request_body})")
             return self.close()
 
         try:
             # Convert the 'type' field to a PacketID
             request_body["type"] = SafeEnginePacketID(request_body["type"])
         except ValueError:
-            logger.warn(f"Undefined request type (given: {request_body['type']})")
+            logger.warning(f"Undefined request type (given: {request_body['type']})")
             return self.close()
 
         from ..handlers.safe_engine import (

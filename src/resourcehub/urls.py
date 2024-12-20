4d6d1ebbc97c69cc2b1d5c6602db02fd69a31b03
GanjaXTn69 <136641689+GanjaXTn69@users.noreply.github.com>
2024-12-20 16:48:35 +0100

    Update urls.py

--- src/resourcehub/urls.py
+++ src/resourcehub/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("scan/multitheftauto", views.download_mta_engine, name="download_agent_peb"),
     path("scan/fivem", views.download_fivem_engine, name="download_fivem_engine"),
 ]

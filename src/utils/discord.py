e6eede024e956b431c174fcbcaddce983e970008
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-29 18:42:20 +0100

    Format discord util

--- src/utils/discord.py
+++ src/utils/discord.py
@@ -14,11 +14,7 @@ async def send_discord_embed(
     color: Optional[nextcord.Color] = nextcord.Color.dark_gold(),
     bot_name: Optional[str] = f"{settings.ANTICHEAT_NAME} AntiCheat",
 ):
-    embed = nextcord.Embed(
-        title=title,
-        description=description,
-        color=color
-    )
+    embed = nextcord.Embed(title=title, description=description, color=color)
     for field in fields:
         embed.add_field(name=field[0], value=field[1], inline=field[2])
 
@@ -29,4 +25,6 @@ async def send_discord_embed(
 
     async with ClientSession() as session:
         webhook = nextcord.Webhook.from_url(webhook_url, session=session)
-        await webhook.send(embed=embed, username=bot_name, avatar_url=settings.BOT_AVATAR_URL)
+        await webhook.send(
+            embed=embed, username=bot_name, avatar_url=settings.BOT_AVATAR_URL
+        )

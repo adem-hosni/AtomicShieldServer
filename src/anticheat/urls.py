b1f703274c2f843339a838eaec6a700bb2c0d851
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-01-04 15:53:27 +0100

    Add agent version check

--- src/anticheat/urls.py
+++ src/anticheat/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("status/agent", view=views.agent_status, name="agent_status"),
     path("status/isconnected", view=views.is_client_connected),
     path("status/server", view=views.server_status, name="server_status"),
+    path("status/version", view=views.version_check),
 ]
--- src/anticheat/views.py
+++ src/anticheat/views.py
@@ -1,6 +1,8 @@
 import json
 from django.http import HttpRequest, HttpResponse
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from utils import check_request_body_key
 from guards import fivem_guard
 
 
@@ -41,6 +43,27 @@ def server_status(request: HttpRequest) -> HttpResponse:
             {
                 "alive": True,
                 "message": "",
+            }
+        )
+    )
+
+
+@csrf_exempt
+def version_check(request: HttpRequest) -> HttpResponse:
+    request_body = json.loads(request.body.decode())
+    if not check_request_body_key(request_body, "version", str):
+        return HttpResponse(json.dumps({"success": False}))
+
+    with open(f"{settings.CONFIG_DIR}\\version.json", "r") as file:
+        version = json.load(file)
+
+    client_version = request_body["version"]
+
+    return HttpResponse(
+        json.dumps(
+            {
+                "success": client_version == version["current_version"]
+                or client_version in version["include"]
             }
         )
     )

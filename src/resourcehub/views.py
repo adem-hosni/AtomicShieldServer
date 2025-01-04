f88be259b0d4aaf5ba73c7fcf01ed36bbe06dc2b
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-01-04 15:13:39 +0100

    Update views.py

--- src/resourcehub/views.py
+++ src/resourcehub/views.py
@@ -2,23 +2,9 @@ import logging
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from safecore import safe_core
 
 
 logger = logging.getLogger(__name__)
-
-
-@csrf_exempt
-def download_mta_engine(request: HttpRequest) -> HttpResponse:
-    with open(
-        f"{settings.BIN_DIR}\\resources\\engines\\multitheftauto.dll",
-        "rb",
-    ) as file:
-        engine_buffer = file.read()
-
-    logger.info(f"{request.get_host()} downloaded MTA:SA engine")
-
-    return HttpResponse(engine_buffer, content_type="application/octet-stream")
 
 
 @csrf_exempt

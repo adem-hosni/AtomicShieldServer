050f118850995abc7fe2ab1ca5591aed6db98d90
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-01-18 22:24:50 +0100

    Update signals.py

--- src/dashboard/signals.py
+++ src/dashboard/signals.py
@@ -8,4 +8,8 @@ from django.contrib.auth.base_user import AbstractBaseUser
 @receiver(user_logged_in)
 def user_login(sender, request: HttpRequest, user: AbstractBaseUser, **kwargs):
     if not request.session.get("selected_server"):
-        request.session["selected_server"] = GameServer.objects.filter(owner=user).last().id
+        last_game_server = GameServer.objects.filter(owner=user).last()
+        if last_game_server:
+            request.session["selected_server"] = last_game_server.id
+        else:
+            request.session["selected_server"] = None

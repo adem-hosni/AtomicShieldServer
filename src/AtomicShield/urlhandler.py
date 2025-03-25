ced9fc391309f1ad9b79a27df9c1312ed492f85f
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-03-25 01:31:34 +0100

    Add http status code 500 handler

--- src/AtomicShield/urlhandler.py
+++ src/AtomicShield/urlhandler.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def handler404(request: HttpRequest, exception: Exception, template_name: str = None):
     return render(request, "pages/404.jinja", {}, status=404)
+
+
+def handler500(request, template_name="500.html"):
+    return render(request, "pages/500.jinja", {}, status=500)

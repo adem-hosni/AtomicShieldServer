d2de239d5236998d0d3517cd97b17cd384626c5b
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-06-30 14:59:00 +0100

    Update run script

--- scripts/runserver.bat
+++ scripts/runserver.bat
@@ -1,3 +1,3 @@
 call "%~dp0..\venv\Scripts\activate.bat"
 cd /d "%~dp0..\src"
-python manage.py runserver
+hypercorn AtomicShield.asgi:application -b 0.0.0.0:8000 -w 8 --keep-alive 5 --graceful-timeout 30

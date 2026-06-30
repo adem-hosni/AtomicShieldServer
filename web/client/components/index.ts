15bd4946f88a8f9793e2019ca07442410d484e2b
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-06-30 17:37:18 +0100

    Remove web directory

--- web/client/components/index.ts
+++
@@ -1,7 +0,0 @@
-// Discord Sign-In Components
-export { default as DiscordSignInButton } from "./DiscordSignInButton";
-export { default as BuilderDiscordSignIn } from "./BuilderDiscordSignIn";
-
-// Re-export with named exports for convenience
-export { DiscordSignInButton } from "./DiscordSignInButton";
-export { BuilderDiscordSignIn } from "./BuilderDiscordSignIn";
--- web/server/standalone.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from "./index";
-
-const app = createServer();
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Express server running on http://localhost:${PORT}`);
-  console.log(`📍 API endpoints available at http://localhost:${PORT}/api`);
-});
